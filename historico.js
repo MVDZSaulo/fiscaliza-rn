b190f782e1fe9f6f33360722929b0d18c5986f6b
MVDZSaulo <araujosaulo3005@gmail.com>
2025-08-02 16:17:39 -0300

    Update historico.js

--- historico.js
+++ historico.js
@@ -1,18 +1,13 @@
-// Carrega denúncias do localStorage
 function carregarHistorico() {
     const denuncias = JSON.parse(localStorage.getItem('denuncias')) || [];
     const tabela = document.getElementById('tabelaDenuncias');
-
-    denuncias.forEach(denuncia => {
-        const tr = document.createElement('tr');
-        tr.innerHTML = `
+    tabela.innerHTML = denuncias.map(denuncia => `
+        <tr>
             <td>${denuncia.id}</td>
             <td>${denuncia.nomeDocumento}</td>
             <td>${denuncia.resultado}</td>
             <td>${denuncia.data}</td>
-        `;
-        tabela.appendChild(tr);
-    });
+        </tr>
+    `).join('');
 }
-
 carregarHistorico();
